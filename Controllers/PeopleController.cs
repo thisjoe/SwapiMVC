fd562e31439118fba28f6f1275f4a189da778e12
Joe Lee <thisjoelee@gmail.com>
2023-10-05 21:20:45 -0400

    Injected into PeopleController

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -1,9 +1,16 @@
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SwapiMVC.Controllers
 {
     public class PeopleController
     {
+        private readonly HttpClient _httpClient;
+        public PeopleController(IHttpClientFactory httpClientFactory)
+        {
+
+        }
+        
         public IActionResult Index()
         {
             return View();
--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient("swapi", client =>
+{
+    client.BaseAddress = new Uri("https://swapi.dev/api/");
+});
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
