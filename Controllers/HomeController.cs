939bb534275cec00ba8f070ef2cbe8e450c25cd7
Joe Lee <thisjoelee@gmail.com>
2023-10-05 21:02:01 -0400

    refactored Index.cshtml and HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace SwapiMVC.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private static Random _random = new Random();
 
     public HomeController(ILogger<HomeController> logger)
     {
@@ -15,7 +16,9 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        string[] names = new[] { "Autumn", "Alecx", "Tom", "Kenn", "Paul"};
+        string name = names[_random.Next(0, names.Length)];
+        return View(model: name);
     }
 
     public IActionResult Privacy()
